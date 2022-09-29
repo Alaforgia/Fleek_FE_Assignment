04666411953149962743a8a873daf60463a854ca
Tony LaForgia <alforgia428@gmail.com>
2022-09-29 15:33:45 -0500

    Added MUI card

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import * as React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+// import theme from "../src/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    //<ThemeProvider>
+      <Component {...pageProps} />
+    //</ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
