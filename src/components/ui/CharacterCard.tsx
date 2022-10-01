7ccd61556479f715da54bb0c27cf7f848bc3ab05
Tony LaForgia <alforgia428@gmail.com>
2022-09-30 22:54:21 -0500

    styling added to card

--- src/components/ui/CharacterCard.tsx
+++ src/components/ui/CharacterCard.tsx
@@ -3,38 +3,99 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+import { CardHeader } from "@mui/material";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { getCharacter } from "../../utils/characters";
+import { blueGrey } from "@mui/material/colors";
+import { flexbox } from "@mui/system";
+import { relative } from "path";
 
 const rick = getCharacter(1);
 
-const bull = (
-  <Box component="span" sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}>
-    •
-  </Box>
-);
-
 const card = (
   <React.Fragment>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Character Logo
-      </Typography>
-      <Typography variant="h5" component="div">
-        be{bull}nev{bull}o{bull}lent
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        adjective
-      </Typography>
-      <Typography variant="body2">
-        well meaning and kindly.
+    <CardHeader
+      sx={{
+        p: 0,
+        mb: 3,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        bgcolor: "limegreen",
+        height: 65,
+      }}
+      // component={Typography}
+      title={
+        <Typography
+          sx={{
+            fontSize: 19,
+            textAlign: "center",
+            p: 0,
+            mb: 0,
+          }}
+          color="text.secondary"
+          // gutterBottom
+        >
+          Character Logo
+        </Typography>
+      }
+    />
+    <CardContent
+      sx={{ p: 0, display: "flex", flexDirection: "column", alignItems: "flex-start", justifyContent: "center" }}
+    >
+      <Typography
+        component="div"
+        color="text.secondary"
+        sx={{ color: "black", fontWeight: "bold", fontSize: 18, pb: 1.5, ml: 1.5 }}
+      >
+        Name
         <br />
-        {'"a benevolent smile"'}
+      </Typography>
+      <Typography
+        component="div"
+        color="text.secondary"
+        sx={{ color: "black", fontWeight: "bold", fontSize: 18, pb: 1.5, ml: 1.5 }}
+      >
+        Specie
+        <br />
+      </Typography>
+      <Typography
+        component="div"
+        color="text.secondary"
+        sx={{ color: "black", fontWeight: "bold", fontSize: 18, pb: 0, ml: 1.5 }}
+      >
+        Status
       </Typography>
     </CardContent>
-    <CardActions>
-      <Button size="small">Learn More</Button>
+    <CardActions
+      sx={{
+        display: "flex",
+        flexWrap: "nowrap",
+        position: "relative",
+        // flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        bottom: "-11.1%",
+        // alignContent: "flex-end",
+        // alignSelf: "flex-end",
+        p: 1,
+        bgcolor: "gray",
+      }}
+    >
+      <Button
+        sx={{
+          position: "relative",
+          bgcolor: "red",
+          border: "1px solid red",
+          borderRadius: 2,
+          p: 1.5,
+          m: 1.5,
+          alignContent: "center",
+        }}
+      >
+        Details
+      </Button>
     </CardActions>
   </React.Fragment>
 );
@@ -42,10 +103,24 @@ const card = (
 export default function CharacterCards() {
   return (
     <>
-      <Box sx={{ minWidth: 275 }}>
-        <Card variant="outlined">{card}s</Card>
-      </Box>
-      <div>{rick}</div>
+      <Card
+        variant="outlined"
+        sx={{
+          // display: "flex",
+          // alignItems: "center",
+          // justifyContent: "center",
+          // flexDirection: "column",
+          width: 420,
+          height: 320,
+          // textAlign: "center",
+          border: 0,
+          borderRadius: 5.5,
+        }}
+      >
+        {card}
+      </Card>
+
+      {/* <div>{rick}</div> */}
     </>
   );
 }
