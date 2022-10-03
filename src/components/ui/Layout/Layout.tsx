cd931f85f7028529baef0ceab4bb00dc6292f285
Tony LaForgia <alforgia428@gmail.com>
2022-10-02 19:12:56 -0500

    Layout component created

---
+++ src/components/ui/Layout/Layout.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
+
+const BackButton = ()=>{
+  return(
+    <>
+    
+    </>
+  )
+}
+
+
+
+export default function Layout() {
+  return (
+    <div>Layout</div>
+  )
+}
