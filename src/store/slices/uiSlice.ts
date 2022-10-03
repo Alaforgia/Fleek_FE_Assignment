a072fc19c4b8d8829508be4bdad119f5abd11c5f
Tony LaForgia <alforgia428@gmail.com>
2022-10-03 18:21:52 -0500

    zod

---
+++ src/store/slices/uiSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "../store";
+
+import { UiSliceState } from "../../types";
+
+const initialState: UiSliceState = {
+  isSidebarOpen: false,
+};
+
+export const uiSlice = createSlice({
+  name: "ui",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    // is state implied here without having to declare it because of toolkit?
+    setIsSidebarOpen(state, action: PayloadAction<boolean>) {
+      state.isSidebarOpen = action.payload;
+    },
+  },
+});
+
+export const { setIsSidebarOpen } = uiSlice.actions;
+
+export const IsSideBarOpen = (state: RootState) => state.ui.isSidebarOpen;
+
+export default uiSlice.reducer;
