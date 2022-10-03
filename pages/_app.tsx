83a89783bc5f6d20d2f52979111516dad52a4212
Tony LaForgia <alforgia428@gmail.com>
2022-10-02 19:10:13 -0500

    app.tsx cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import * as React from "react";
-import { ThemeProvider } from "@mui/material/styles";
-// import theme from "../src/theme";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    //<ThemeProvider>
+    
       <Component {...pageProps} />
-    //</ThemeProvider>
+  
   );
 }
 
