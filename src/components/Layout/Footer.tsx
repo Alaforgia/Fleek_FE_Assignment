3fc4e55e1bb07057fb1cc9e5596639a849f55c12
Tony LaForgia <alforgia428@gmail.com>
2022-10-04 00:22:40 -0500

    footer update

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,23 @@
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Copyright from "./Copyright";
+
+const Footer: React.FC = () => {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        py: 3,
+        px: 2,
+        mt: "auto",
+        backgroundColor: (theme) => theme.palette.primary.main,
+      }}
+    >
+      <Container maxWidth="sm">
+        <Copyright />
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
