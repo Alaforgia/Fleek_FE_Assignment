cd80f7e7967f5e2d36d79531ef6130cd8700f6ff
Tony LaForgia <alforgia428@gmail.com>
2022-10-04 01:17:58 -0500

    styling

--- src/components/CharacterDetails/CharacterDetailsGrid.tsx
+++ src/components/CharacterDetails/CharacterDetailsGrid.tsx
@@ -13,58 +13,59 @@ import { selectedCharacterDetails } from "../../store/slices/rickAndMortySlice";
 const CharacterDetailsGrid = () => {
   const characterDetails = useAppSelector(selectedCharacterDetails);
   return (
-    <Grid
-      container
-      spacing={{ xs: 2, md: 3 }}
-      columns={{ xs: 4, sm: 8, md: 12 }}
-    >
+    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
       {characterDetails?.name && (
         <Grid item xs={2} sm={4} md={12}>
-          <Typography variant="h5" component="h5">
+          <Typography variant="h5" component="h5" sx={{ ml: 3 }}>
+            Name: &nbsp;&nbsp;
             {characterDetails.name}
           </Typography>
         </Grid>
       )}
       {characterDetails?.status && (
         <Grid item xs={2} sm={4} md={12}>
-          <Typography variant="h5" component="h5">
+          <Typography variant="h5" component="h5" sx={{ ml: 3 }}>
+            Status: &nbsp;&nbsp;
             {characterDetails.status}
           </Typography>
         </Grid>
       )}
       {characterDetails?.species && (
         <Grid item xs={2} sm={4} md={12}>
-          <Typography variant="h5" component="h5">
+          <Typography variant="h5" component="h5" sx={{ ml: 3 }}>
+            Species: &nbsp;&nbsp;
             {characterDetails.species}
           </Typography>
         </Grid>
       )}
       {characterDetails?.type && (
         <Grid item xs={2} sm={4} md={12}>
-          <Typography variant="h5" component="h5">
+          <Typography variant="h5" component="h5" sx={{ ml: 3 }}>
+            Type: &nbsp;&nbsp;
             {characterDetails.type}
           </Typography>
         </Grid>
       )}
       {characterDetails?.gender && (
         <Grid item xs={2} sm={4} md={12}>
-          <Typography variant="h5" component="h5">
+          <Typography variant="h5" component="h5" sx={{ ml: 3 }}>
+            Gender: &nbsp;&nbsp;
             {characterDetails.gender}
           </Typography>
         </Grid>
       )}
-      {characterDetails?.origin &&
-        characterDetails?.origin?.name &&
-        characterDetails?.origin?.url && (
-          <Grid item xs={2} sm={4} md={12}>
-            <Typography variant="h5" component="h5">
-              {characterDetails?.origin?.name}
-            </Typography>
-          </Grid>
-        )}
+      {characterDetails?.origin && characterDetails?.origin?.name && characterDetails?.origin?.url && (
+        <Grid item xs={2} sm={4} md={12}>
+          <Typography variant="h5" component="h5" sx={{ ml: 3 }}>
+            Origin: &nbsp;&nbsp;
+            {characterDetails?.origin?.name}
+          </Typography>
+        </Grid>
+      )}
       {characterDetails?.created && (
         <Grid item xs={2} sm={4} md={12}>
-          <Typography variant="h5" component="h5">
+          <Typography variant="h5" component="h5" sx={{ ml: 3 }}>
+            Date Created: &nbsp;&nbsp;
             {characterDetails.created.slice(0, 10)}
           </Typography>
         </Grid>
